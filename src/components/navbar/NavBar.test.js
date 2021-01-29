4475dd83c77801c7e0a3abadd701a51af5ee7670
Maria Tibau <maria.ta.89@gmail.com>
2021-01-29 21:47:49 +0100

    Add test data to tests all link on navbar

--- src/components/navbar/NavBar.test.js
+++ src/components/navbar/NavBar.test.js
@@ -5,16 +5,29 @@ import { MemoryRouter } from 'react-router';
 
 describe("NavBar", () => {
 
-    test('Should have home link', () => {
+    const expectedLinks = [
+        { text: 'Inicio', path: "/home"},
+        { text: 'Sobre mi', path: "/aboutme"},
+        { text: 'Masajes', path: "/massages"},
+        { text: 'Tarifas', path: "/prices"},
+        { text: 'Oferta', path: "/offer"},
+        { text: 'Contacto', path: "/contact"},
+
+    ]
+
+    test('Should have expected links', () => {
         render(
-            <MemoryRouter initialEntries={[ '/home' ]}>
+            <MemoryRouter>
                 <NavBar />
             </MemoryRouter>);
 
-        expect(screen
-            .getByText('Inicio')
-            .closest('a'))
-            .toHaveAttribute('href', '/home')
+        expectedLinks.forEach(link => {
+            expect(screen
+                .getByText(link.text)
+                .closest('a'))
+                .toHaveAttribute('href', link.path)
+        })
+
 
     })
 })

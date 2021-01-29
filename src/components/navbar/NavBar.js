08925a4b560e4c10e5955d5c7c15218fd767bd0f
Maria Tibau <maria.ta.89@gmail.com>
2021-01-29 21:48:36 +0100

    Add links on navbar to pass tests

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,6 +6,11 @@ const NavBar = () => {
     return (
         <nav className="navbar">
             <NavLink className="navbar__link" exact to="/home">Inicio</NavLink>
+            <NavLink className="navbar__link" exact to="/aboutme">Sobre mi</NavLink>
+            <NavLink className="navbar__link" exact to="/massages">Masajes</NavLink>
+            <NavLink className="navbar__link" exact to="/prices">Tarifas</NavLink>
+            <NavLink className="navbar__link" exact to="/offer">Oferta</NavLink>
+            <NavLink className="navbar__link" exact to="/contact">Contacto</NavLink>
         </nav>
     )
 }

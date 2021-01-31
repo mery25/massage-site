b0d03b05c38809ef79e85b5d5ff624bdd1bba875
Maria Tibau <maria.ta.89@gmail.com>
2021-01-31 17:16:38 +0100

    Add AboutMe to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import './App.sass';
 import NavBar from "./navbar/NavBar"
+import AboutMe from "./aboutme/AboutMe"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <main>
+        <AboutMe />
+      </main>
     </div>
   );
 }

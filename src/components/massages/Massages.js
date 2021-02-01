adc5ac9dcc66ea3a7a6025bba1ebc6054876c533
Maria Tibau <maria.ta.89@gmail.com>
2021-02-01 11:51:12 +0100

    Refactor massage info into Massage component

--- src/components/massages/Massages.js
+++ src/components/massages/Massages.js
@@ -1,7 +1,49 @@
 import React from "react"
 import "./Massages.sass"
+import Massage from "./massage/Massage"
 
 const Massages = () => {
+
+    const massages = [
+        {
+            id: 0,
+            tabName: "Ayurveda",
+            title: "Masaje ayurveda",
+            image: "./ayurveda-pic.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dicta quam aspernatur quos cum explicabo dignissimos unde distinctio commodi inventore?",
+            features: [
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet."
+            ]
+        },
+        {
+            id: 1,
+            tabName: "Descontracturante",
+            title: "Masaje relajante descontracturante",
+            image: "./descontracturante-pic.jpg",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dicta quam aspernatur quos cum explicabo dignissimos unde distinctio commodi inventore?",
+            features: [
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet.",
+                "Lorem ipsum dolor sit amet."
+            ]
+        }
+    ]
+
+    const buildDisplayedTabContent = () => {
+        //TODO change to use state
+        const massage = massages[0]
+        return (
+            <Massage 
+                title={massage.title}
+                image={massage.image}
+                description={massage.description}
+                features={massage.features}
+            />
+        )
+    }
+
     return (
         <section className="massages">
             <h2>Massages</h2>
@@ -10,28 +52,7 @@ const Massages = () => {
                 <button className="massages__tab">Descontracturante</button>
             </div>
             <div className="massages__tab-content">
-                <div className="massage">
-                    <h3 className="massage__name">Masaje ayurveda</h3>
-                    <img className="massage__image" alt="ayurveda_pic.jpg"></img>
-                    <p className="massage__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dicta quam aspernatur quos cum explicabo dignissimos unde distinctio commodi inventore?</p>
-                    <ul className="massage__features">
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                    </ul>
-                </div>
-                <div className="massage">
-                    <h3 className="massage__name">Masaje relajante descontracturante</h3>
-                    <img className="massage__image" alt="descontracturante_pic.jpg"></img>
-                    <p className="massage__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dicta quam aspernatur quos cum explicabo dignissimos unde distinctio commodi inventore?</p>
-                    <ul className="massage__features">
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                    </ul>
-                </div>
+                {buildDisplayedTabContent()}
             </div>
         </section>
     )

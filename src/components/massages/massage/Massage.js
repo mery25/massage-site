fa61aa73b8f342aa134ce22d7fe4932ff4b3b3be
Maria Tibau <maria.ta.89@gmail.com>
2021-02-01 12:37:39 +0100

    Fix identation

--- src/components/massages/massage/Massage.js
+++ src/components/massages/massage/Massage.js
@@ -5,15 +5,15 @@ const Massage = ({title, image, description, features}) => {
 
     return (
         <div className="massage">
-        <h3 className="massage__name">{title}</h3>
-        <img className="massage__image" src={image} alt={title}></img>
-        <p className="massage__description">{description}</p>
-        <ul className="massage__features">
-            {
-                features.map( (feature, idx) => <li key={idx}>{feature}</li>)
-            }
-        </ul>
-    </div>
+            <h3 className="massage__name">{title}</h3>
+            <img className="massage__image" src={image} alt={title}></img>
+            <p className="massage__description">{description}</p>
+            <ul className="massage__features">
+                {
+                    features.map( (feature, idx) => <li key={idx}>{feature}</li>)
+                }
+            </ul>
+        </div>
     )
 }
 

5ca66f68d8e1fe384bd106696374eaf94d04b5e5
Maria Tibau <maria.ta.89@gmail.com>
2021-02-01 09:35:23 +0100

    Add two buttons to the Massages component

---
+++ src/components/massages/Massages.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Massages = () => {
+    return (
+        <section className="massages">
+            <button>Ayurveda</button>
+            <button>Descontracturante</button>
+        </section>
+    )
+}
+
+export default Massages

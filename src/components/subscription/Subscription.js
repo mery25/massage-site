a43a71a58cf9b991f5ab38adaad52ce25d229797
Maria Tibau <maria.ta.89@gmail.com>
2021-02-01 19:21:08 +0100

    Add logic to call subscribe email on submit

--- src/components/subscription/Subscription.js
+++ src/components/subscription/Subscription.js
@@ -1,23 +1,32 @@
 import React, {useState} from "react"
 import "./Subscription.sass"
 
-const Subscription = () => {
+const Subscription = ({validateEmail, subscribeEmail}) => {
 
     const [subscriptionEmail, setSubscriptionEmail] = useState("")
+
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        subscribeEmail(subscriptionEmail)
+    }
 
     return (
         <section className="subscription">
             <h3>SUSCRIBETE A MI NEWSLELLETER PARA RECIBIR OFERTAS ESPECIALES</h3>
-            <form>
+            <form onSubmit={onSubmit} >
                 <input 
                     type="email" 
                     placeholder="Escribe tu email" 
                     aria-label="email-input"
                     value={subscriptionEmail} 
-                     onChange={e => setSubscriptionEmail(e.currentTarget.value)} 
+                    onChange={e => setSubscriptionEmail(e.currentTarget.value)} 
                 />
                 <small></small>
-                <input type="submit" value="+" alt="Enviar" />
+                <input 
+                    type="submit" 
+                    value="+" 
+                    alt="Enviar"
+                />
             </form>
         </section>
     )

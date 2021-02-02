7b783c3b05ca36b1653f387d9a42182386c4b939
Maria Tibau <maria.ta.89@gmail.com>
2021-02-02 19:57:49 +0100

    Add logic to return null when phone validation is valid

--- src/validators.js
+++ src/validators.js
@@ -1,7 +1,11 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const PHONE_REGEX = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
 
 const isEmailValid = (email) => {
     return EMAIL_REGEX.test(email)
+}
+const isPhoneValid = (phone) => {
+    return PHONE_REGEX.test(phone)
 }
 
 export const emailValidation = email => {
@@ -12,4 +16,10 @@ export const emailValidation = email => {
         return "Email is required";
     }
     return "Please enter a valid email";
+}
+
+export const phoneValidation = phone => {
+    if (isPhoneValid(phone)) {
+        return null;
+    }
 }

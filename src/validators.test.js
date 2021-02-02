213f2dfc6415a1f24e93eb41f4c053b53a26af82
Maria Tibau <maria.ta.89@gmail.com>
2021-02-02 19:57:00 +0100

    Test phone validation return null when is valid

--- src/validators.test.js
+++ src/validators.test.js
@@ -1,26 +1,41 @@
 import "./validators"
-import { emailValidation } from "./validators";
+import { emailValidation, phoneValidation } from "./validators";
 
-describe("Email validation logic", () => {
+describe("Validations logic", () => {
+    describe("Email validation logic", () => {
 
-    test('Should return null when the email is valid', () => {
-        const validEmail = 'm@gmail.com'
-        expect(emailValidation(validEmail)).toBeNull()
+        test('Should return null when the email is valid', () => {
+            const validEmail = 'm@gmail.com'
+            expect(emailValidation(validEmail)).toBeNull()
+        })
+
+        test('Should return `Email is required` when email is empty', () => {
+            let emptyEmail = ' '
+            expect(emailValidation(emptyEmail)).toBe('Email is required')
+        })
+
+        test('Should return `Please enter a valid email` when email is invalid', () => {
+            let invalidEmail = 'a.gm.c'
+            expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
+
+            invalidEmail = '@gm.c'
+            expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
+
+            invalidEmail = 'mery@'
+            expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
+        })
     })
 
-    test('Should return `Email is required` when email is empty', () => {
-        let emptyEmail = ' '
-        expect(emailValidation(emptyEmail)).toBe('Email is required')
-    })
+    describe("Phone validation logic", () => {
+        test('Should return null when the phone is valid', () => {
+            let validPhone = '+34 677453289'
+            expect(phoneValidation(validPhone)).toBeNull()
 
-    test('Should return `Please enter a valid email` when email is invalid', () => {
-        let invalidEmail = 'a.gm.c'
-        expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
+            validPhone = '677453289'
+            expect(phoneValidation(validPhone)).toBeNull()
 
-        invalidEmail = '@gm.c'
-        expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
-
-        invalidEmail = 'mery@'
-        expect(emailValidation(invalidEmail)).toBe('Please enter a valid email')
+            validPhone = '0034677453289'
+            expect(phoneValidation(validPhone)).toBeNull()
+        })
     })
 })

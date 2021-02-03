046f96328d57c61ec5fe134ed434c15f536fefee
Maria Tibau <maria.ta.89@gmail.com>
2021-02-03 15:05:45 +0100

    Add Contact component to App

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,20 @@ import AboutMe from "./aboutme/AboutMe"
 import Massages from "./massages/Massages"
 import Offers from "./offers/Offers"
 import Products from "./products/Products"
+import Subscription from "./subscription/Subscription"
+import Contact from "./contact/Contact"
+import { emailValidation, phoneValidation } from '../validators'
 
 function App() {
+
+  const subscribeEmail = (email) => {
+    console.log("subscribe email "  + email)
+  }
+
+  const sendContact = (contactInfo) => {
+    console.dir(contactInfo, {depth: 1})
+  }
+
   return (
     <div className="App">
       <NavBar />
@@ -14,9 +26,11 @@ function App() {
         <Massages />
         <Offers />
         <Products />
+        <Subscription validateEmail={emailValidation} subscribeEmail={subscribeEmail} />
+        <Contact validateEmail={emailValidation} validatePhone={phoneValidation} sendContact={sendContact}/>
       </main>
     </div>
-  );
+  )
 }
 
 export default App

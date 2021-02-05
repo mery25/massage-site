835c9237738544cb442c82d69500aaadc51ebb94
Maria Tibau <maria.ta.89@gmail.com>
2021-02-05 12:20:57 +0100

    Remove semicolon

--- src/hooks/useWindowSize.test.js
+++ src/hooks/useWindowSize.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { act } from 'react-dom/test-utils';
+import { act } from 'react-dom/test-utils'
 import useWindowSize from './useWindowSize'
 
 const fireResize = (width, height) => {

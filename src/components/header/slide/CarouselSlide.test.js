ca197f3993f0d30acf0d49a4ac8185b97fb8b77e
Maria Tibau <maria.ta.89@gmail.com>
2021-02-06 20:45:10 +0100

    Test the image is centered when screen is resized

--- src/components/header/slide/CarouselSlide.test.js
+++ src/components/header/slide/CarouselSlide.test.js
@@ -57,4 +57,37 @@ describe("CarouselSlide", () => {
         expect(img.className).toContain('carousel-slide__image--landscape')
 
     })
+
+    test('Should change image position when resize screen', () => {
+        const image = {
+            src: "",
+            alt: "Image"
+        }
+        const { rerender } = render(<CarouselSlide image={image} />);
+
+        const img = screen
+            .getByAltText('Image')
+            .closest('img')
+
+        fireResize(345, 500)
+        rerender(<CarouselSlide image={image} />)
+        expect(img.style).toHaveProperty('bottom', '0px')
+        expect(img.style).toHaveProperty('left', '-202.5px')
+
+        fireResize(690, 234)
+        rerender(<CarouselSlide image={image} />)
+        expect(img.style).toHaveProperty('bottom', '-113px')
+        expect(img.style).toHaveProperty('left', '0px')
+
+        fireResize(1024, 690)
+        rerender(<CarouselSlide image={image} />)
+        expect(img.style).toHaveProperty('bottom', '0px')
+        expect(img.style).toHaveProperty('left', '-5.5px')
+
+        fireResize(1024, 650)
+        rerender(<CarouselSlide image={image} />)
+        console.log(img.style)
+        expect(img.style).toHaveProperty('bottom', '-16.33px')
+        expect(img.style).toHaveProperty('left', '0px')
+    })
 })

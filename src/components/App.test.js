2953236cc15dfbf5817f52c612ffeb9c5126d027
Maria Tibau <maria.ta.89@gmail.com>
2021-02-08 21:02:39 +0100

    Remove Router from the app

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,7 @@
 import { render } from '@testing-library/react';
 import App from './App';
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 
 test('Renders App sucessfully', () => {
-  render(
-    <MemoryRouter>
-        <App />
-    </MemoryRouter>);
+  render(<App />);
 });

55c2a27d79b0e3ac839ed0a8ef11e85f738aff13
Maria Tibau <maria.ta.89@gmail.com>
2021-02-08 20:58:54 +0100

    Change Navbar to use scroll link instead of router links

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,32 +1,131 @@
-import React, {useState} from "react"
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react"
+import { Link } from 'react-scroll'
 import ToggleNav from './toggle_nav/ToggleNav'
 import "./NavBar.sass"
 
 const NavBar = () => {
 
-    const [ isNavBarCollapsed, setIsNavBarCollapsed ] = useState(true);
+    const [isNavBarCollapsed, setIsNavBarCollapsed] = useState(true);
 
-    const toggleNavbar = () => setIsNavBarCollapsed( isNavBarCollapsed => !isNavBarCollapsed )
+    const toggleNavbar = () => setIsNavBarCollapsed(isNavBarCollapsed => !isNavBarCollapsed)
 
+    const navBarScrollConfig = {
+        spy: true,
+        smooth: true,
+        duration: 500,
+        offset: -80
+    }
+
+    const {spy, smooth, duration, offset} = navBarScrollConfig
     return (
-        <div className="navbar">
-            <ToggleNav 
-                active={!isNavBarCollapsed} 
+        <nav className="navbar">
+            <ToggleNav
+                active={!isNavBarCollapsed}
                 toggle={toggleNavbar}
             />
-            <NavLink className="navbar__logo" exact to="/home">
+            <Link
+                className="navbar__logo"
+                activeClass="navbar__logo--active"
+                to="header"
+                spy={spy}
+                smooth={smooth}
+                duration={duration}
+            >
                 <img alt="Logo" />
-            </NavLink>
-            <nav data-testid="navbar-nav" className={ isNavBarCollapsed ? " navbar__nav--collapsed" : "navbar__nav" }>
-                <NavLink className="navbar__link" exact to="/home">Inicio</NavLink>
-                <NavLink className="navbar__link" exact to="/aboutme">Sobre mi</NavLink>
-                <NavLink className="navbar__link" exact to="/massages">Masajes</NavLink>
-                <NavLink className="navbar__link" exact to="/prices">Tarifas</NavLink>
-                <NavLink className="navbar__link" exact to="/offer">Oferta</NavLink>
-                <NavLink className="navbar__link" exact to="/contact">Contacto</NavLink>
-            </nav>
-        </div>
+            </Link >
+            <ul data-testid="navbar-nav" className={isNavBarCollapsed ? " navbar__nav--collapsed" : "navbar__nav"}>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="header"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                    >
+                        Inicio
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="aboutme"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Sobre mi
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="massages"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Masajes
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="offer"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Ofertas
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="products"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Tarifas
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="testimonials"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Testimonios
+                    </Link >
+                </li>
+                <li className="navbar__item">
+                    <Link
+                        className="navbar__link"
+                        activeClass="navbar__link--active"
+                        to="contact"
+                        spy={spy}
+                        smooth={smooth}
+                        duration={duration}
+                        offset={offset}
+                    >
+                        Contacto
+                    </Link >
+                </li>
+            </ul>
+        </nav>
     )
 }
 

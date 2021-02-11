b34fb570ed30c455658e826d1be2032f23976b8f
Maria Tibau <maria.ta.89@gmail.com>
2021-02-11 12:19:21 +0100

    Refactor Product component to use common components

--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -1,18 +1,13 @@
 import React from "react"
 import "./Product.sass"
+import {List} from "../../common"
 
 const Product = ({title, priceAmount, features}) => {
     return (
         <div className="product">
             <h3 className="product__title">{title}</h3>
             <label className="product__price-amount">{priceAmount}€</label>
-            <ul className="product__features">
-                {
-                    features.map( (feature, idx) => 
-                        <li className="product__feature" key={idx}>{feature}</li>
-                    )
-                }
-            </ul>
+            <List className="product__features" items={features}/>
         </div>
     )
 }

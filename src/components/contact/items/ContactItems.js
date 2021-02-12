7254d489572b9488eda2f5fce2dc7399a4e1db33
Maria Tibau <maria.ta.89@gmail.com>
2021-02-12 12:28:43 +0100

    Add another map and direction on the contact items

--- src/components/contact/items/ContactItems.js
+++ src/components/contact/items/ContactItems.js
@@ -15,15 +15,30 @@ const ContactItems = () => {
                 text="+34 677673123"
                 icon={faPhoneAlt} 
             />
+            <a className="contact-items__link" href="maito:cris.gu@gmail.com">
+                <ContactItem
+                    text="cris.guz@gmail.com"
+                    icon={faEnvelope} 
+                />
+            </a>
             <ContactItem
-                text="cris.guz@gmail.com"
-                icon={faEnvelope} 
+                text="Calle 5, 56 Valencia"
+                icon={faMapMarkerAlt} 
+            />
+            <IFrame
+                className="contact-items__map"
+                title="Mapa de Valencia"
+                source="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1537.306310689679!2d-0.4616216619124423!3d39.590880092311274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd605cc7cf2903df%3A0x2ee3c82f25c67a13!2zQ2FsbGUgSm9zw6kgR2FzY8OzbiwgNjAsIDQ2MTE3IELDqXRlcmEsIFZhbGVuY2lh!5e0!3m2!1ses!2ses!4v1612270227167!5m2!1ses!2ses"
+                width="600" 
+                height="450"
             />
             <ContactItem
                 text="Calle José Gascón, 60  Bétera, Valencia"
                 icon={faMapMarkerAlt} 
             />
             <IFrame
+                className="contact-items__map"
+                title="Mapa de Valencia"
                 source="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1537.306310689679!2d-0.4616216619124423!3d39.590880092311274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd605cc7cf2903df%3A0x2ee3c82f25c67a13!2zQ2FsbGUgSm9zw6kgR2FzY8OzbiwgNjAsIDQ2MTE3IELDqXRlcmEsIFZhbGVuY2lh!5e0!3m2!1ses!2ses!4v1612270227167!5m2!1ses!2ses"
                 width="600" 
                 height="450"
